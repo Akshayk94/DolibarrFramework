03fda4b38a30b31c9ac70af2fa787004d9b8b83f
chetanghodekar <chetanghodekar7875@gmail.com>
2024-06-01 12:47:36 +0530

    Sales Order Draft Sales Order script Completion with hard core values

--- src/main/java/org/dolibarr/objectRepository/CommerceDashboard.java
+++ src/main/java/org/dolibarr/objectRepository/CommerceDashboard.java
@@ -1,8 +1,38 @@
 package org.dolibarr.objectRepository;
 
+import org.dolibarr.base.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class CommerceDashboard {
+public class CommerceDashboard extends BasePage {
+
+    @FindBy(xpath = "//a[text()='Sales Orders']")
+    WebElement salesOrderLink;
+
+    @FindBy(linkText = "List")
+    WebElement salesOrderListLink;
+
+    @FindBy(linkText = "New order")
+    WebElement newSalesOrderLink;
+
     public CommerceDashboard(WebDriver driver) {
+        super(driver);
     }
+
+    public SalesOrderDashBoard clickOnSalesOrder(){
+        salesOrderLink.click();
+        return new SalesOrderDashBoard(driver);
+    }
+
+    public SalesOrderList clickOnSalesOrderList(){
+        salesOrderListLink.click();
+        return new SalesOrderList(driver);
+    }
+
+    public NewSalesOrderPage clickNewSalesOrder(){
+        newSalesOrderLink.click();
+        return new NewSalesOrderPage(driver);
+    }
+
 }
